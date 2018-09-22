6b619dbdb603927e756c8e6e27ef68eda4ea31a0
Moonlock <moonlock@riseup.net>
2018-09-21 21:12:47 -0600

    Allow configuration through commandline options

--- src/learn_morse.py
+++ src/learn_morse.py
@@ -2,4 +2,14 @@
 
 from learnMorse import learnMorse
 
-learnMorse.morseTest(20, 10, 2, 300)
+DEFAULT_CHAR_WPM = 20
+DEFAULT_OVERALL_WPM = 10
+DEFAULT_NUM_CHARS = 2
+DEFAULT_TEST_TIME = 300
+
+charWpm = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_CHAR_WPM
+overallWpm = sys.argv[2] if len(sys.argv) > 2 else DEFAULT_OVERALL_WPM
+numChars = sys.argv[3] if len(sys.argv) > 3 else DEFAULT_NUM_CHARS
+testTime = sys.argv[4] if len(sys.argv) > 4 else DEFAULT_TEST_TIME
+
+learnMorse.morseTest(charWpm, overallWpm, numChars, testTime)
