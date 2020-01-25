1e3200b75212c829b4ad38d146e2814a9a90f7db
Moonlock <moonlock@riseup.net>
2020-01-25 00:02:11 -0600

    Add explanation for single/multiple destinations in setup.py.

--- setup.py
+++ setup.py
@@ -22,9 +22,21 @@ def main():
 		serverConfig['WPM'] = input('WPM playback speed [20]: ') or 20
 		serverConfig['Port'] = input('Local port [8000]: ') or 8000
 
-		multiDest = input('Enable multiple destinations [y/N]: ') or 'n'
-		multiDest = multiDest.lower() == 'y'
+		print()
+		print("    S: Single contact")
+		print("        Configured in this script.")
+		print("        To change the destination server you will have to rerun this")
+		print("        script and restart your server.")
+		print("    M: Multiple contacts")
+		print("        Allows sending telegrams to various contacts without")
+		print("        restarting the server.")
+		print("        Contacts are identified by a callsign that must be sent in the message.")
+		destMode = ""
+		while destMode != 's' and destMode != 'm':
+			destMode = input('Destination mode: ').lower()
+		multiDest = destMode == 'm'
 		clientConfig['Multiple Destinations'] = multiDest
+
 		if not multiDest:
 			clientConfig['Address'] = input('Remote IP address/hostname [localhost]: ') or 'localhost'
 			clientConfig['Port'] = input('Remote port [8000]: ') or 8000
@@ -42,7 +54,7 @@ def main():
 
 	if multiDest:
 		print("")
-		print("Use `./configureContacts` to create contacts and groups.")
+		print("Use `./configureContacts.py` to create contacts and groups.")
 
 if __name__ == '__main__':
 	main()
