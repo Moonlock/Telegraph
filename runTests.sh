c56ee43f849da498794d1ee67507088adf24a128
Moonlock <moonlock@riseup.net>
2020-01-24 23:36:02 -0600

    Don't run tests with GPIO by default.

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-python3 -m unittest `find test -name "test_*.py"`
+if [ "$1" = "all" ]; then
+	# Until I get the telegraph running on not-Pis,
+	# don't run tests requiring GPIO by default.
+	python3 -m unittest `find test -name "test_*.py"`
+else
+	python3 -m unittest test.test_commonFunctions test.telegraph.test_destinationConfig
+fi
