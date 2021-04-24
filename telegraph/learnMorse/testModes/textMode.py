f96d66d77039a5d1b9268ce19c70909a1373abb8
Moonlock <moonlock@riseup.net>
2021-04-23 20:10:53 -0600

    Fixes for text test.
    - Fix crashing when starting on whitespace or reaching end of text
    - Fix using copyright in test

--- telegraph/learnMorse/testModes/textMode.py
+++ telegraph/learnMorse/testModes/textMode.py
@@ -15,6 +15,7 @@ import telegraph.common.commonFunctions as common
 
 COUNTS_PER_WORD = 50
 SECONDS_PER_MINUTE = 60
+APPROX_CHARS_PER_WORD = 5
 
 DIT_FILE = SOUND_FILES_PATH + "learnMorse-dit.sox"
 DAH_FILE = SOUND_FILES_PATH + "learnMorse-dah.sox"
@@ -24,6 +25,9 @@ WORD_SPACE_FILE = SOUND_FILES_PATH + "learnMorse-word.sox"
 TEST_FILE = SOUND_FILES_PATH + "learnMorse-test.sox"
 
 TEXT_FILES_PATH = "resources/testData/corpus/"
+
+# Offset in text files to skip copyright/table of contents
+TEXT_START_OFFSET = 200
 
 
 class TextMode(TestModeInterface):
@@ -50,6 +54,7 @@ class TextMode(TestModeInterface):
 		Popen(['sox', '-n', WORD_SPACE_FILE, 'trim', '0', str(self.wordSpace)])
 
 		self.testTime = testTime
+		self.wpm = overallWpm
 
 		sleep(2)
 		self.startTest()
@@ -76,14 +81,17 @@ class TextMode(TestModeInterface):
 		timeSpent = 0
 		fileList = []
 
+		approxCharsInTest = self.wpm*APPROX_CHARS_PER_WORD * self.testTime/SECONDS_PER_MINUTE
+
 		text = self.getText()
-		pos = random.choice(range(len(text)))
+		maxStartingPos = int(len(text) - approxCharsInTest*2)
+		pos = random.choice(range(maxStartingPos - TEXT_START_OFFSET)) + TEXT_START_OFFSET
 
 		while timeSpent < self.testTime:
 
 			char = text[pos]
 
-			if (char == ' ' or char == '\n') and self.masterList[-1] != ' ':
+			if (char == ' ' or char == '\n' or char == '-') and self.masterList and self.masterList[-1] != ' ':
 				fileList.append(WORD_SPACE_FILE)
 				timeSpent += self.wordSpace
 				self.masterList.append(' ')
